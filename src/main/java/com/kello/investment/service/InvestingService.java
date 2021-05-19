77f8956e7930581e2b4c5424940fab3dcde7f68e
HK <hyoin01@gmail.com>
2021-05-19 20:41:36 +0900

    fix. modify transactional

--- src/main/java/com/kello/investment/service/InvestingService.java
+++ src/main/java/com/kello/investment/service/InvestingService.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -54,7 +55,7 @@ public class InvestingService {
         .build();
   }
 
-  @Transactional
+  @Transactional(isolation = Isolation.READ_COMMITTED)
   public CommonResponse<InvestingStatusDto> invest(long userId, long productId, long amount) {
     checkValidRequest(productId, amount);
 
